ad5752094db10fb13b17cf9e7269c905aec2ecec
simo-hr <xxbbxx825@gmail.com>
2026-02-07 08:55:24 +0900

    サイトのグローバル定数を整理し、BaseHeadコンポーネントにメタデータを追加。JSON-LD構造化データを実装し、Twitterカード情報を更新。

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,21 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+/**
+ * サイト全体で使用するグローバル定数
+ *
+ * @description サイト全体で共有される設定値を定義
+ * @limitations なし
+ */
 
+/** サイトタイトル */
 export const SITE_TITLE = 'simo blog';
-export const SITE_DESCRIPTION = 'プログラミングや技術に関する個人ブログ';
+
+/** サイト説明文 */
+export const SITE_DESCRIPTION = '個人ブログ';
+
+/** サイトURL */
+export const SITE_URL = 'https://simo-blog.com';
+
+/** Twitter/Xアカウント */
+export const TWITTER_ACCOUNT = '@hiro_nr825';
+
+/** 著者名 */
+export const AUTHOR_NAME = 'simo';
